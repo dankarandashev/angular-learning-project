13a234ea1a7ad2686b389f716db091e53f9313f2
dkaranda <denis.karandashev@t-systems.com>
2022-05-09 14:00:01 +0300

    feat: @ViewChild + nativeElement

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,24 +1,45 @@
-import { Component, Input, Output, EventEmitter, TemplateRef, ViewChild, ElementRef, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, TemplateRef, ViewChild, ElementRef, OnChanges, SimpleChanges, AfterViewInit } from '@angular/core';
 
 @Component({
 	selector: 'app-sidenav',
 	templateUrl: './sidenav.component.html',
 	styleUrls: ['./sidenav.component.less'],
 })
-export class SidenavComponent implements OnChanges {
-
+export class SidenavComponent implements OnChanges, AfterViewInit {
 	@Input() isOpened = true;
 	@Input() navListTemplate!: TemplateRef<unknown>;
 	@Output() isOpenedChange = new EventEmitter<boolean>();
 
-  @ViewChild('navListContainer')
-  private navListContainer!: ElementRef;
+	// essentially replacing the query-selector; if you need access from the ngOnInit method, you must specify static: true
+	@ViewChild('navListContainer')
+	private navListContainer!: ElementRef<HTMLElement>;
 
-  ngOnChanges({navListTpl}: SimpleChanges): void {
-    throw new Error('Method not implemented.');
-  }
+	/*******************************
+	 * NG HOOKS
+	 *******************************/
 
-	onToggleSidenav() {
+	ngOnChanges({ navListTemplate }: SimpleChanges): void {
+		if (navListTemplate) {
+			this.viewNavListTemplate();
+		}
+	}
+
+	ngAfterViewInit(): void {
+		console.log(this.navListContainer);
+		// nativeElement is a regular DOM element
+		this.navListContainer.nativeElement.style.width = '100px';
+		this.navListContainer.nativeElement.style.height = '100px';
+		this.navListContainer.nativeElement.style.background = 'red';
+	}
+
+
+	/*******************************
+	 * HELPERS
+	 *******************************/
+
+	private onToggleSidenav(): void {
 		this.isOpenedChange.emit(!this.isOpened);
 	}
+
+	private viewNavListTemplate(): void { }
 }
